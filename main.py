64a0a3b800c99bfa0c0cb3e12999f66c3efe0808
sako-egs <137052619+sako-egs@users.noreply.github.com>
2023-06-19 18:57:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,24 +5,25 @@ st.set_page_config(layout="wide")
 st.markdown('## ✏️ IELTS Writing Task2 Evaluation System with ChatGPT')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type="password")
+temperature = st.sidebar.slider('回答のランダム性(0に近いほど回答は安定)', 0.0, 1.0, 0.0)
 
-def generate_response(input_prompt):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+def generate_response(temperature, input_prompt):
+  llm = OpenAI(temperature=temperature, openai_api_key=openai_api_key)
   st.info(llm(input_prompt))
 
 with st.form('my_form'):
     question = st.text_area('Input a task:', '')
     essay = st.text_area(f'Input your essay:', '', height=250)
-    submitted_ct = st.form_submit_button('word count')
-    if submitted_ct and essay:
-        words_ct = len(essay.split())
-        st.write(f'現在{words_ct}文字')
-    else:
-        st.write(f'現在0文字')
+    if st.form_submit_button('word count'):
+        if essay:
+            words_ct = len(essay.split())
+            st.write(f'現在{words_ct}文字')
+        else:
+            st.write(f'現在0文字')
   
     prompt =f"""
     You know everything about scoring IELTS essays. You assess the given essay of the given question and provide feedback and specify mistakes and suggest corrections.
-    Your output must be a JSON following this structure: ｛"band": the band, "feedback": your feedback (maximum 100 words), "mistakes":[｛"mistake 1": the whole sentence,"correction 1": a correction for that sentence｝,｛"mistake 2": ・・・｝]｝
+    Your output must be a JSON following this structure: ｛"band": the band, "feedback": your feedback (maximum 100 words), "mistakes":[｛"mistake": the whole sentence,"correction": a correction for that sentence｝,｛"mistake":...,"correction":...｝,]｝
   
     Question: {question}
   
@@ -39,4 +40,4 @@ with st.form('my_form'):
             st.warning('質問あるいはエッセイが入力されていません。', icon='⚠') 
         else:
             with st.spinner("評価中..."):
-                generate_response(prompt)
+                generate_response(temperature, prompt)
